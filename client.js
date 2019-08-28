1ad9b14c71092c48a1f59df6333834fde3996320
vagrant <Kennethzhang374@gmail.com>
2019-08-28 21:15:43 +0000

    all good now yeah

--- client.js
+++ client.js
@@ -2,14 +2,23 @@ const net = require('net');
 
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '192.160.88.151',
+    host: '192.168.88.151',
     port: 50541
     
   });
   
+  conn.on('connect', () => {
+    conn.write('Name: KEN');
+  });//print out name sending server message
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+  });///print connecting successfully message
+
   conn.on('data', (data) => {
     console.log('Server says: ', data);
-  });
+  });///catch server message
+
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
