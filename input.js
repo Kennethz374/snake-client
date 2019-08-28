512e1763d7d91bbab453a6cba2c74273070e44b9
vagrant <Kennethzhang374@gmail.com>
2019-08-28 23:09:20 +0000

    finally done

--- input.js
+++ input.js
@@ -1,13 +1,26 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.on('data', (key) => {
     if (key === '\u0003') { //control + c
       process.exit();
-    }});
+    } if (key === "w") {
+      conn.write("Move: up");
+    } if (key === "s") {
+      conn.write("Move: down");
+    } if (key === "a") {
+      conn.write("Move: left");
+    } if (key === "d") {
+      conn.write("Move: right");
+    }
+
+  });
   stdin.resume();
   return stdin;
 };//stdin setup
 
-module.export = { setupInput };
+module.exports = { setupInput };
